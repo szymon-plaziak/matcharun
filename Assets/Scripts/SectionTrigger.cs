2dbe5587e72c5d8f27fcc45beabe6ae104c4034e
Szymon Płaziak <164235578+szymon-plaziak@users.noreply.github.com>
2026-06-19 13:41:47 +0200

    Initial check-in

---
+++ Assets/Scripts/SectionTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SectionTrigger : MonoBehaviour
+{
+
+    public GameObject roadSection;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Trigger"))
+        {
+            Instantiate(roadSection, new Vector3(-57,0,0), Quaternion.identity);
+        }
+    }
+}
